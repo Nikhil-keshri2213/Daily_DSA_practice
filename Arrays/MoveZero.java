2b81f0e639399bc202da354c7fd904698d4703d2
Nikhil Keshri <95970358+Nikhil-keshri2213@users.noreply.github.com>
2025-12-09 15:05:04 +0000

    Day-2, 9 Dec 2025, Done

--- Arrays/MoveZero.java
+++ Arrays/MoveZero.java
@@ -1,3 +1,28 @@
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the 
+relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+Concept : Two Pointer, In-Place change.
+*/
+
 public class MoveZero {
-    
+    public static void moveZero(int arr[]){
+        int left = 0;
+        for(int right = 0; right<arr.length; right++){
+            if(arr[right] !=0){
+                int temp = arr[right];
+                arr[right] = arr[left];
+                arr[left] = temp;
+                left++;
+            }
+        }   
+    }
+    public static void main(String[] args) {
+       int[] nums = {0,1,0,3,12};
+       moveZero(nums);
+       for (int i : nums) {
+        System.out.print(i+", ");
+       }
+    }
 }
--- Arrays/TwoSum2.java
+++ Arrays/TwoSum2.java
@@ -10,16 +10,36 @@ added by one as an integer array [index1, index2] of length 2.
 The tests are generated such that there is exactly one solution. 
 You may not use the same element twice.
 
-Your solution must use only constant extra space. */
+Your solution must use only constant extra space. 
+concept : Two Pointer
+*/
 
 public class TwoSum2 {
-    public static void twoSum2(int arr[], int target){
+    public static int[] twoSum2(int arr[], int target){
+        int[] res = new int[2];
+        int left = 0;
+        int right = arr.length - 1;
 
+        while(left < right){
+            int sum = arr[left] + arr[right];
+            if(sum == target){
+                res[0] = left + 1;
+                res[1] = right + 1;
+                System.out.println(res[0] + ", "+res[1]); //Testing
+                return res;
+            }
+            if(sum > target){
+                right--;
+            }else{
+                left++;
+            }
+        }
+        System.out.println(res[0] + ", "+res[1]);
+        return res;
     }
     public static void main(String[] args) {
         int arr[] = {2,7,11,15};
         int target = 9;
-
         twoSum2(arr, target);
     }
 }
